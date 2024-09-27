fbb29711e5d43e34f9d864b553182f67455a495a
联想 <1335245845@qq.com>
2024-09-27 11:16:46 +0800

    应急资源信息-接口更改

--- src/api/system/emergency.js
+++ src/api/system/emergency.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getEmergency(query) {
     return request({
-        url: '/system/emergency/getEmergency',
+        url: '/emergencyResources/getEmergency',
         method: 'get',
         params: query
     })
