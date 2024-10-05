3ee7fc8c41a37c654ef0b4a63c3d77a30873b53c
Alexcarvenxie <859942718@qq.com>
2024-10-05 11:41:37 +0800

    9.28 时间轴列表bug

--- src/components/TimeLine/news.vue
+++ src/components/TimeLine/news.vue
@@ -69,7 +69,7 @@ export default {
     'eqid'
   ],
   mounted() {
-      // console.log("this.eqid1111111111111------------",this.eqid)
+      console.log("this.eqid1111111111111------------",this.eqid)
       if(this.eqid === 'be3a5ea4-8dfd-a0a2-2510-21845f17960b'){
           this.ifShowData = true
           this.fetchData()
