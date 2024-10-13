5c0f3d3f9aeaaccd48c1fe0a002bd68e44ff2dcc
3323442749 <3323442749@qq.com>
2024-10-13 10:39:08 +0800

    2024/10/13 救援出队、人员伤亡表和标绘数据联动bug

--- src/api/system/timeLine.js
+++ src/api/system/timeLine.js
@@ -10,16 +10,26 @@ export function getEmergencyResponse() {
 }
 // 获取 人员伤亡
 export function getRescueActionCasualties() {
+    // 获取URL中的参数
+    const urlParams = new URLSearchParams(window.location.search);
+    const eqid = urlParams.get('eqid');  // 从URL中获取eqid参数
+
     return request({
         url: '/timeLine/rescueActionCasualties',
         method: 'get',
-    })
+        params: { eqid }  // 传递获取到的eqid
+    });
 }
+
 // 获取 救援出队
 export function getRescueTeam() {
+    // 获取URL中的参数
+    const urlParams = new URLSearchParams(window.location.search);
+    const eqid = urlParams.get('eqid');  // 从URL中获取eqid参数
     return request({
         url: '/timeLine/rescueTeam',
         method: 'get',
+        params: { eqid }  // 传递获取到的eqid
     })
 }
 // 获取 新闻
