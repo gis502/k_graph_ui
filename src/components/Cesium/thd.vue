c3e92258f0ea8fc492db220864da9c0bcb7bf29c
联想 <1335245845@qq.com>
2024-12-07 01:37:01 +0800

    指挥大屏--图件产出：更名图件产出

--- src/components/Cesium/thd.vue
+++ src/components/Cesium/thd.vue
@@ -101,7 +101,7 @@
     <div v-if="activeComponent === 'thematicMapDownload'" class="thd-listTable ">
       <div class="pop" style="width: 100%;height: 100%; z-index: 900; ">
         <div class="pop_header">
-          <span class="pop_title">专题图产出</span>
+          <span class="pop_title">图件产出</span>
         </div>
         <div class="list-dialog__content" style="height: calc(100% - 40px);">
 
