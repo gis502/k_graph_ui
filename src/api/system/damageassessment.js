2b0958b1185c3b1fd9dfe4857270bab048d02763
xiaodemos <11165536+xiaodemos@user.noreply.gitee.com>
2025-02-16 21:41:32 +0800

    专题图、灾情报告产出速度优化

--- src/api/system/damageassessment.js
+++ src/api/system/damageassessment.js
@@ -32,6 +32,22 @@ export function getEqOutputReport(eqOutputReportDTO) {
   })
 }
 
+export function getEqOutputMap(eqId,eqqueueId) {
+    return request({
+        url: '/tp/api/open/eq/map/start',
+        method: 'get',
+        params: { eqId, eqqueueId}
+    })
+}
+
+export function getEqOutputReport(eqId,eqqueueId) {
+    return request({
+        url: '/tp/api/open/eq/report/start',
+        method: 'get',
+        params: { eqId, eqqueueId}
+    })
+}
+
 export function getPersonDes(eqid) {
     return request({
         url: '/damageassessment/getPersonDes',
