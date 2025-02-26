4075949212bbb380c55350fc5683b9bee69b37df
Smile <3039126061@qq.com>
2025-02-26 16:21:22 +0800

    20240224地震信息更新

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,24 +1,24 @@
-export const domainName = "http://59.213.183.7";
+// export const domainName = "http://59.213.183.7";
 // export const domainName = "http://172.26.86.82";
-//  export const domainName = "http://10.16.7.69/zaisunimage";
+ export const domainName = "http://10.16.7.69/zaisunimage";
 
 
- // export const tianDitulocalApi="http://api.tianditu.gov.cn"
-export const tianDitulocalApi="http://59.213.183.7/tiandituapi"
- // export const tianditu="http://t0.tianditu.com"
-export const tianditu="http://59.255.48.160:81"
+ export const tianDitulocalApi="http://api.tianditu.gov.cn"
+// export const tianDitulocalApi="http://59.213.183.7/tiandituapi"
+ export const tianditu="http://t0.tianditu.com"
+// export const tianditu="http://59.255.48.160:81"
 //
-export const AmapApiLocal="http://59.213.183.7/restapi/v3"
- // export const AmapApiLocal="https://restapi.amap.com/v3"
+// export const AmapApiLocal="http://59.213.183.7/restapi/v3"
+ export const AmapApiLocal="https://restapi.amap.com/v3"
 
- // export const webSocketLocal ="localhost:8080"
-export const webSocketLocal ="59.213.183.7/websocket"
+ export const webSocketLocal ="localhost:8080"
+// export const webSocketLocal ="59.213.183.7/websocket"
 //
 //
-// export const zaisunipLocal = "http://10.16.7.69/zaisun"
-export const zaisunimageipLocal = "http://59.213.183.7/jcpt"
+export const zaisunipLocal = "http://10.16.7.69/zaisun"
+// export const zaisunimageipLocal = "http://59.213.183.7/jcpt"
 //
-// export const zaisunimageipLocal = "http://10.16.7.69/zaisunimage"
+export const zaisunimageipLocal = "http://10.16.7.69/zaisunimage"
 
 // export const SystemIP = "http://59.213.183.7/prod-api"
 // export const SystemIPLocal = "http://localhost:8080"
